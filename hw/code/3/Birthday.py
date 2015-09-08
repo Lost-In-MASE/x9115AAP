bae240f66159c35758b86244dfefcaa45643444e
Aaditya Sriram <aadisriram@gmail.com>
2015-09-07 22:01:17 -0400

    Added partial probability calculation for poker

--- hw/code/3/Birthday.py
+++ hw/code/3/Birthday.py
@@ -1,1 +1,32 @@
-__author__ = 'aadisriram'
+import random
+
+
+# 10.15 Exercise 8
+
+
+def has_duplicates(input_list):
+    input_set = set()
+    for input in input_list:
+        if input not in input_set:
+            input_set.add(input)
+        else:
+            return True
+
+    return False
+
+
+def get_random_birthdays():
+    birthday_list = []
+    for i in range(23):
+        birthday_list.append(random.randint(1, 365))
+
+    return birthday_list
+
+
+def calculate_probability():
+    count = 0
+    for i in range(1000):
+        if has_duplicates(get_random_birthdays()):
+            count += 1
+
+    return count/float(1000)
--- hw/code/3/Employee.py
+++ hw/code/3/Employee.py
@@ -1,1 +1,10 @@
-__author__ = 'aadisriram'
+class Employee:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __repr__(self):
+        return 'Name: %s, Age: %d' %(self.name, self.age)
+
+    def __lt__(self, other):
+        return self.age < other.age
